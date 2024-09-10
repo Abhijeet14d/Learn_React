de39213d9aeac63a65aa36a1fb3d68acfd5a88de
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2024-09-11 01:00:53 +0530

    add card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Counter from './components/Counter';
+import Card from './components/Card';
 
 function App() {
 
@@ -6,8 +6,10 @@ function App() {
 
   return (
     <div className="App">
-          <h1>Hello Abhijeet!!</h1>
-          <Counter/>
+          <h1 className="bg-green-500 text-black p-4 mb-4 rounded-xl ">Hello Abhijeet!!</h1>
+
+          <Card username = "Abhijeet" btnText = "click me"/>
+          <Card username = "Pratimesh" btnText = "Fuck me"/>
           
     </div>
   );
