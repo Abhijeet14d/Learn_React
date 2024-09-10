e8f34911effd2e631fb930c3b8f4a94d861364bc
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2024-09-11 01:50:08 +0530

    add BgChange component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Card from './components/Card';
+import BgChange from "./components/Bg_Change";
 
 function App() {
-
   
-
   return (
-    <div className="App">
-          <h1 className="bg-green-500 text-black p-4 mb-4 rounded-xl ">Hello Abhijeet!!</h1>
-
-          <Card username = "Abhijeet" btnText = "click me"/>
-          <Card username = "Pratimesh" btnText = "Fuck me"/>
-          
-    </div>
+    <BgChange />
   );
 }
 
